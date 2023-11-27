31cdd0ca4cc6e54d4ce16e4df48e896d9dc9c709
hassa <hassa@LAPTOP-U03CN6LR>
2023-11-27 03:42:22 +0330

    Revert "congestion history update"
    This reverts commit 6ea69d09138754642c84afa9386617a1704e5581.

--- CongestionTaxCalculator.Api/Controllers/v1/CongestionTaxHistoryController.cs
+++ CongestionTaxCalculator.Api/Controllers/v1/CongestionTaxHistoryController.cs
@@ -1,6 +1,7 @@
 ﻿using CongestionTaxCalculator.Application.DTOs.CongestionTaxHistory;
+using CongestionTaxCalculator.Application.Features.City.Requests.Queries;
+using CongestionTaxCalculator.Application.Features.CongestionTaxCal.Requests.Queries;
 using CongestionTaxCalculator.Application.Features.CongestionTaxHistory.Requests.Commands;
-using CongestionTaxCalculator.Application.Features.CongestionTaxHistory.Requests.Queries;
 using CongestionTaxCalculator.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
